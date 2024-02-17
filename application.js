960e71e3b5b29b950ae42b02266e6adf380a152f
grant-luna <grantlunafidelity@gmail.com>
2024-02-17 15:53:55 -0800

    Configure view templates and view location

--- application.js
+++ application.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Configure View Engine
 app.set('view engine', 'pug');
+app.set('views', ['./views', './views/components']);
 
 // Configure Logging
 const morgan = require('morgan');
 app.use(morgan('common'));
+
+// Configure Static Files
+app.use(express.static('public'));
 
