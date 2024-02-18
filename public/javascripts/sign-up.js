83779358604baf216e0e16bda0fa8ee2b532760f
grant-luna <grantlunafidelity@gmail.com>
2024-02-18 13:28:53 -0800

    Initialize sign-up page client-side JavaScript for future use

---
+++ public/javascripts/sign-up.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const signUpButton = document.querySelector('button.submit');
+  signUpButton.addEventListener('click', (event) => {
+    
+  });
+});
