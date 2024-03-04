c911ca29c9c9a6d6767e54d14c53a165c50c06ff
grant-luna <grantlunafidelity@gmail.com>
2024-03-03 17:14:38 -0800

    Move handleNewSearchClick from ClientWorker to SearchesClientWorker

--- public/javascripts/client-worker.js
+++ public/javascripts/client-worker.js
@@ -39,31 +39,6 @@ export class ClientWorker {
     }
   }
 
-  static async handleNewSearchClick(event) {
-    event.stopPropagation();
-
-    let newSearchMenu = document.querySelector('.new-search-menu');
-
-    if (!newSearchMenu) {
-      try {
-        const response = await fetch('/newSearchWindow')
-        if (!response.ok) throw new Error('Unable to fetch the New Search window');
-        const newSearchHtml = await response.text();
-        if (!newSearchHtml) throw new Error('Unable to create the New Search menu');
-        
-        const newSearchMenu = document.createElement('div');
-        newSearchMenu.classList.add('new-search-menu');
-        newSearchMenu.innerHTML = newSearchHtml;
-
-        ClientWorker.attachNewSearchWindowEventListeners(newSearchMenu);
-        
-        document.querySelector('main').appendChild(newSearchMenu);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
-
   static async handleSignOut(event) {
     try {
       const response = await fetch('/sign-out', { method: 'POST' });
--- public/javascripts/searches.js
+++ public/javascripts/searches.js
@@ -5,9 +5,48 @@ document.addEventListener('DOMContentLoaded', async () => {
   searchesContainer.addEventListener('click', ClientWorker.handleUserSearchSelection);
 
   const newSearchContainer = document.querySelector('.new-search-container')
-  newSearchContainer.addEventListener('click', ClientWorker.handleNewSearchClick)
+  newSearchContainer.addEventListener('click', SearchesClientWorker.handleNewSearchClick)
 });
 
 class SearchesClientWorker extends ClientWorker {
+  static attachNewSearchWindowEventListeners(newSearchMenu) {
+    const closeButton = newSearchMenu.querySelector('img');
+    closeButton.addEventListener('click', SearchesClientWorker.handleCloseNewSearchMenu)
+    
+    const fileInput = newSearchMenu.querySelector('input[type="file"]');
+    fileInput.addEventListener('change', ClientWorker.verifyFileFormat);
+    
+    const uploadButton = newSearchMenu.querySelector('button.submit');
+    uploadButton.addEventListener('click', ClientWorker.handleUploadFile);
+  }
+
+  static handleCloseNewSearchMenu(event) {
+    const newSearchMenu = document.querySelector('.new-search-menu');
+    newSearchMenu.remove();
+  }
   
+  static async handleNewSearchClick(event) {
+    event.stopPropagation();
+
+    let newSearchMenu = document.querySelector('.new-search-menu');
+
+    if (!newSearchMenu) {
+      try {
+        const response = await fetch('/newSearchWindow')
+        if (!response.ok) throw new Error('Unable to fetch the New Search window');
+        const newSearchHtml = await response.text();
+        if (!newSearchHtml) throw new Error('Unable to create the New Search menu');
+        
+        const newSearchMenu = document.createElement('div');
+        newSearchMenu.classList.add('new-search-menu');
+        newSearchMenu.innerHTML = newSearchHtml;
+
+        SearchesClientWorker.attachNewSearchWindowEventListeners(newSearchMenu);
+        
+        document.querySelector('main').appendChild(newSearchMenu);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
