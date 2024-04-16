992fd22eb033b9584f37c84978b500981dc7a80b
grant-luna <grantlunafidelity@gmail.com>
2024-04-16 13:20:52 -0700

    Add initial Faster FastPeopleSearch functionality

--- app/fasterFastPeopleSearch/page.jsx
+++ app/fasterFastPeopleSearch/page.jsx
@@ -1,3 +1,58 @@
+"use client"
+import { useState } from 'react';
+import Papa from "papaparse";
+
 export default function Page() {
-  return <h1>Faster FastPeopleSearch</h1>;
+  const [ parsedFile, setParsedFile ] = useState(null);
+
+  function handleFileSelection(event) {
+    const selectedFile = event.target.files[0];
+    
+    Papa.parse(selectedFile, {
+      header: true,
+      complete: (result) => {
+        setParsedFile(result);
+      },
+      error: () => {
+        console.log('Error reading file');
+      }
+    })
+  }
+  
+  return (
+    <>
+      <h1>Faster FastPeopleSearch</h1>
+      {!parsedFile && <input type='file' accept='.csv' onChange={handleFileSelection}></input>}
+      {parsedFile && <FileMatcher parsedFile={parsedFile} setParsedFile={setParsedFile}/>}
+    </>
+  )
+}
+
+// https://www.fastpeoplesearch.com/address/3445-gurnard-ave_san-pedro-ca
+// https://www.fastpeoplesearch.com/address/3529-e-broadway-7_long-beach-ca
+function FileMatcher({ parsedFile, setParsedFile }) {
+  const [ siteAddress, setSiteAddress ] = useState(undefined);
+  const [ mailAddress, setMailAddress ] = useState(undefined);
+  const [ ownerName, setOwnerName ] = useState(undefined);
+
+  return (
+    <>
+      <h3>File Matcher</h3>
+      {!siteAddress && <p>{JSON.stringify(Object.keys(parsedFile.data[0]))}</p>}
+      {siteAddress && !mailAddress && <HeaderMatchBox />}
+      {siteAddress && mailAddress && <HeaderMatchBox/>}
+      <div>
+        <button type="button" onClick={() => setParsedFile(false)}>Start Over</button>
+        <button type="button" disabled={true}>Finish</button>
+      </div>
+    </>
+  )
+}
+
+function HeaderMatchBox() {
+  return (
+    <>
+      
+    </>
+  )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@ export default function Page() {
   return (
     <>
       <h1>User Dashboard</h1>
+      <div>
+        <h2>Faster FastPeopleSearch Overview</h2>
+      </div>
     </>
   )
 }
