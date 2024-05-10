1772c0bde328dd5657b0efa0d4f543b078e3b09d
grant-luna <grantlunafidelity@gmail.com>
2024-05-09 18:04:08 -0700

    Made various changes to file upload process

--- app/fasterFastPeopleSearch/page.jsx
+++ app/fasterFastPeopleSearch/page.jsx
@@ -2,6 +2,7 @@
 import styles from './page.module.css';
 import { useState, useRef } from 'react';
 import Papa from "papaparse";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function MainContent() {
   const [ parsedFile, setParsedFile ] = useState(null);
@@ -33,25 +34,6 @@ function FileMatchMenu({ parsedFile, setParsedFile }) {
     { type: 'Mail Address', address: [], cityState: [], completable: false }
   ]);
 
-  const [ isClicked, setIsClicked ] = useState(false);
-  const [ currentSelectedProperty, setCurrentSelectedProperty ] = useState('');
-
-  function handleInputTypeClick(event) {
-    const newSelectedProperty = event.currentTarget.textContent;
-
-    if (currentSelectedProperty === newSelectedProperty) {
-      setIsClicked(!isClicked);
-      setCurrentSelectedProperty('');
-    } else if (isClicked) {
-      setCurrentSelectedProperty(newSelectedProperty);
-    } else {
-      setIsClicked(!isClicked);
-      setCurrentSelectedProperty(newSelectedProperty);
-    }
-  }
-
-  const fileMatchMenuProps = { inputTypes, setInputTypes, currentSelectedProperty };
-
   return (
     <>
       <h3>File Matcher</h3>
@@ -66,18 +48,23 @@ function FileMatchMenu({ parsedFile, setParsedFile }) {
         information for the primary address, owner names, and mail
         address (if they exist).
       </p>
-      {isClicked && <HeaderMatcher parsedFile={parsedFile} fileMatchMenuProps={fileMatchMenuProps} />}
-      <div class="accordion" id="accordionExample">
-        < AccordionItem itemName={"Primary Address"} toggleId={"collapseOne"} parsedFile={parsedFile} />
-        < AccordionItem itemName={"Owner Names"} toggleId={"collapseTwo"} parsedFile={parsedFile}/>
-        < AccordionItem itemName={"Mail Address"} toggleId={"collapseThree"} parsedFile={parsedFile}/>
-      </div>
+      <ul class="accordion" id="accordionExample">
+        < AccordionItem key={uuidv4()} itemName={"Primary Address"} toggleId={"collapseOne"} parsedFile={parsedFile} />
+        < AccordionItem key={uuidv4()} itemName={"Owner Names"} toggleId={"collapseTwo"} parsedFile={parsedFile}/>
+        < AccordionItem key={uuidv4()} itemName={"Mail Address"} toggleId={"collapseThree"} parsedFile={parsedFile}/>
+      </ul>
     </>
   )
 }
 
-function AccordionItem({ itemName, toggleId, parsedFile  }) {
-  const [ isCompleted, setIsCompleted ] = useState(false);
+function AccordionItem({ itemName, toggleId, parsedFile }) {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  function findRequiredHeaders(itemName) {
+    return itemName === 'Owner Names' ? { "First Owner": [], "Second Owner": [] } : { "Address": [], "City / State": [] };
+  }
+
+  const [requiredHeaders, setRequiredHeaders] = useState(findRequiredHeaders(itemName));
 
   const headers = Object.keys(parsedFile[0]);
 
@@ -90,8 +77,46 @@ function AccordionItem({ itemName, toggleId, parsedFile  }) {
   };
 
   const sampleRow = headers.map((header) => {
-    return { header, value: findSampleValue(header, parsedFile) }
+    return { header, value: findSampleValue(header, parsedFile) };
   });
+
+  function generateCurrentMatch(requiredHeaders, requiredHeader, sampleRow) {
+    return requiredHeaders[requiredHeader].map((columnHeader) => {
+      return sampleRow.find((row) => row.header === columnHeader).value;
+    }).join(' ');
+  }
+
+  function checkIfCompleted(itemName) {
+    if (itemName === 'Owner Names') {
+      if (Object.keys(requiredHeaders).some((requiredHeader) => requiredHeaders[requiredHeader].length > 0 )) {
+        setIsCompleted(true);
+      } else {
+        setIsCompleted(false);
+      }
+    } else if (itemName === 'Primary Address' || iteName === 'Mail Address') {
+      if (Object.keys(requiredHeaders).every((requiredHeader) => requiredHeaders[requiredHeader].length > 0)) {
+        setIsCompleted(true);
+      } else {
+        setIsCompleted(false);
+      }
+    }
+  }
+
+  function handleAccordionItemClick(event) {
+    
+  }
+
+  function handleRequiredHeaderClick(event) {
+    event.preventDefault();
+  }
+
+  function handleResetRequiredHeaderMatch(requiredHeader) {
+    setRequiredHeaders({...requiredHeaders, [requiredHeader]: []});
+  }
+
+  function handleCancelRequiredHeaderMatch(requiredHeader) {
+    setRequiredHeaders({...requiredHeaders, [requiredHeader]: []});
+  }
 
   function handleSampleColumnHover(event) {
     const sampleColumn = event.currentTarget;
@@ -107,95 +132,84 @@ function AccordionItem({ itemName, toggleId, parsedFile  }) {
     sampleColumn.style.cursor = 'default';
   }
 
-  return (
-    <div className="accordion-item">
-      <h2 className="accordion-header">
-        <button className="accordion-button collapsed" type="button" 
-                data-bs-toggle="collapse" 
-                data-bs-target={`#${toggleId}`} 
-                aria-expanded="false" 
-                aria-controls={toggleId}>
-          {itemName}
-        </button>
-      </h2>
-      <div id={toggleId} className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-        <div className={`${styles.inputTypeMatcherMenu} accordion-body`}>
-          <div className={styles.fileMatcherOptionsContainer}>
-            <ul className={`${styles.sampleColumns} list-group`}>
-              {sampleRow.map((column, index) => {
-                return (
-                  <li onMouseOver={handleSampleColumnHover} onMouseLeave={handleSampleColumnUnHover} className={`${styles.sampleColumn} list-group-item d-flex justify-content-between align-items-start`} key={index}>
-                    <h4><strong>{column.header}</strong></h4>
-                    <p><strong>Sample Value: </strong>{column.value}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+  function handleSampleColumnClick(requiredHeader, event) {
+    if (event.target.parentNode.tagName === 'LI' || event.target.tagName === 'LI') {
+      event.preventDefault();
 
-
-function HeaderMatcher({ parsedFile, fileMatchMenuProps }) {
-  const [ currentMatch, setCurrentMatch ] = useState('');
-  const [ currentRequiredKey, setCurrentRequiredKey ] = useState('');
-  const { inputTypes, setInputTypes, currentSelectedProperty } = fileMatchMenuProps;
-
-  const propertyToMatch = fileMatchMenuProps.inputTypes.find((inputType) => {
-    return inputType.type === fileMatchMenuProps.currentSelectedProperty;
-  });
-
-  const requiredKeys = Object.keys(propertyToMatch).filter((key) => {
-    return key !== 'type' && key !== 'completable';
-  });
-
-  const headers = Object.keys(parsedFile[0]);
-  console.log(headers);
-  const findSampleValue = (header, parsedFile) => {
-    const matchingRow = parsedFile.find((row) => {
-      return row[header] !== '';
-    });
-    return matchingRow[header] || 'Empty';
-  };
-
-  const sampleRow = headers.map((header) => {
-    return { header, value: findSampleValue(header, parsedFile) }
-  });
-
-  function handleRequiredKeyClick(event) {
-    if (event.target.tagName === 'LI') {
-      // Start Here to begin finding a way to create a sample value when
-      // a use clicks one of the required keys.  I have to imagine it
-      // would require getting the value from sampleRow
-      const sampleValues = propertyToMatch[event.target.textContent].map((columnHeader) => {
-        console.log(sampleRow[columnHeader])
-      });
-      setCurrentRequiredKey(event.target.textContent);
+      const sampleColumn = event.target.closest('li');
+      const [ sampleColumnHeader, sampleColumnValue ] = [...sampleColumn.children].map((child) => child.textContent);
+      const requiredHeadersCopy = { ...requiredHeaders }
+      requiredHeadersCopy[requiredHeader].push(sampleColumnHeader);
+      
+      setRequiredHeaders({ ...requiredHeaders, [requiredHeader]: requiredHeadersCopy[requiredHeader]} );
     }
   }
 
   return (
-    <div>
-      <ul className={styles.requiredKeys} onClick={handleRequiredKeyClick}>
-        {requiredKeys.map((requiredKey, index) => {
-          return <li key={index}>{requiredKey}</li>
-        })}
-      </ul>
-      <div className={styles.fileMatcherOptionsContainer}>
-        <ul className={styles.fileMatcherOptions} onClick={handleHeaderClick}>
-          {sampleRow.map((column, index) => {
-            return (
-              <li key={index} className={styles.fileMatcherOption}>
-                <h4>{column.header}</h4>
-                <p>{column.value}</p>
-              </li>
-            );
-          })}
-        </ul>
+    <div onClick={handleAccordionItemClick} className={`${styles.accordionItem} accordion-item`}>
+      <h2 className="accordion-header">
+        <button className="accordion-button collapsed" type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#${toggleId}`}
+          aria-expanded="false"
+          aria-controls={toggleId}>
+          {`${itemName}: ${isCompleted}`}
+        </button>
+      </h2>
+      <div id={toggleId} className={`accordion-collapse collapse`} data-bs-parent="#accordionExample">
+        <div className={`${styles.inputTypeMatcherMenu} accordion-body`}>
+          <ul className={`${styles.requiredHeaders} list-group`}>
+            {Object.keys(requiredHeaders).map((requiredHeader, index) => {
+              const uniqueKey = uuidv4();
+
+              /*
+              func
+              */
+
+              return (
+                <>
+                  <li key={index} onClick={handleRequiredHeaderClick} className={`${styles.requiredHeader} list-group-item`} data-bs-toggle="modal" data-bs-target={`#matcherModal${uniqueKey}`}>
+                    <input class="form-check-input me-1" type="checkbox" value="" id={`checkbox${uniqueKey}`} />
+                    <label class="form-check-label" for={`checkbox${uniqueKey}`} >{requiredHeader}</label>
+                  </li>
+                  <div className="modal fade" id={`matcherModal${uniqueKey}`} data-bs-backdrop="static">
+                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title">Matching {requiredHeader}</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                        <h6>Current Match: {generateCurrentMatch(requiredHeaders, requiredHeader, sampleRow)}</h6>
+                          <ul onClick={handleSampleColumnClick.bind(null, requiredHeader)} className={`${styles.sampleColumns} list-group`}>
+                            {sampleRow.map((column, index) => {
+                              return (
+                                <li onMouseOver={handleSampleColumnHover}
+                                    onMouseLeave={handleSampleColumnUnHover}
+                                    className={`${styles.sampleColumn} list-group-item d-flex justify-content-between align-items-start`}
+                                    key={index}
+                                  >
+                                  <h4><strong>{column.header}</strong></h4>
+                                  <p>{column.value}</p>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" onClick={(event) => handleResetRequiredHeaderMatch(requiredHeader)} class="btn btn-light">Reset</button>
+                          <button type="button"  onClick={(event) => handleCancelRequiredHeaderMatch(requiredHeader)} class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                          <button type="button" onClick={(event) => checkIfCompleted(itemName, requiredHeader)} data-bs-dismiss="modal" class="btn btn-primary">Save</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
