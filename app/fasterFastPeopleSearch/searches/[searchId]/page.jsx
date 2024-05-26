4e524a74eac966fb6c001851dd3f8b96913c5a6a
grant-luna <grantlunafidelity@gmail.com>
2024-05-25 23:32:47 -0700

    Create Offcanvas functionality for each FastPeopleSearch results page

--- app/fasterFastPeopleSearch/searches/[searchId]/page.jsx
+++ app/fasterFastPeopleSearch/searches/[searchId]/page.jsx
@@ -20,6 +20,7 @@ function SearchResults({ searchId }) {
         });
         const parsedData = await fetchedData.json();
         const searchData = JSON.parse(parsedData["search_data"]);
+        
         setSearchData(searchData);
         setLoading(false);
       } catch (error) {
@@ -31,11 +32,7 @@ function SearchResults({ searchId }) {
 
     fetchSearchData(searchId);
   }, [searchId])
-
-  function handleContactInformationClick(contactInformationLink) {
-    
-  }
-
+  
   return (
     <>
       {!loading && <ul>
@@ -46,13 +43,19 @@ function SearchResults({ searchId }) {
                 <h3>Primary Address</h3>
                 <p><strong>Address: </strong>{searchRow.primaryAddress.address}</p>
                 <p><strong>City / State: </strong>{searchRow.primaryAddress.cityState}</p>
-                <button className="btn btn-primary" type="button" onClick={() => handleContactInformationClick(searchRow.primaryAddressLink)}>Contact Information</button>
+                <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" href={`#searchRowPrimaryAddress${index}Offcanvas`} aria-controls={`searchRowPrimaryAddress${index}Offcanvas`}>
+                  Contact Information
+                </button>
+                <FastPeopleSearchOffcanvas index={index} addressType={"Primary Address"} fastPeopleSearchLink={searchRow.primaryAddressLink} />
               </div>
               <div>
                 <h3>Mail Address</h3>
                 <p><strong>Address: </strong>{searchRow.mailAddress.address}</p>
                 <p><strong>City / State: </strong>{searchRow.mailAddress.cityState}</p>
-                <button className="btn btn-primary" type="button" onClick={() => handleContactInformationClick(searchRow.mailAddressLink)}>Contact Information</button>
+                <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" href={`#searchRowMailAddress${index}Offcanvas`} aria-controls={`searchRowPrimaryAddress${index}Offcanvas`}>
+                    Contact Information
+                </button>
+                <FastPeopleSearchOffcanvas index={index} addressType={"Mail Address"} fastPeopleSearchLink={searchRow.mailAddressLink} />
               </div>
               <div>
                 <h3>Owner Name</h3>
@@ -64,5 +67,31 @@ function SearchResults({ searchId }) {
         })};
       </ul>}
     </>
+  )
+}
+
+function FastPeopleSearchOffcanvas({ index, addressType, fastPeopleSearchLink}) {
+  return (
+    <div className="offcanvas offcanvas-start" id={`searchRow${addressType.replace(/ /g, '')}${index}Offcanvas`}>
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <div>
+          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+        <div className="dropdown mt-3">
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
