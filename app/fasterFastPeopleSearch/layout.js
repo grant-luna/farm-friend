8c16ae186b8cbcd0e8f2e8181ae47873e562d5c3
grant-luna <grantlunafidelity@gmail.com>
2024-06-03 17:39:59 -0700

    Make minor style changes to navbar

--- app/fasterFastPeopleSearch/layout.js
+++ app/fasterFastPeopleSearch/layout.js
@@ -6,29 +6,37 @@ export default function RootLayout({ children }) {
       <div className={`navbar`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Faster FastPeopleSearch</a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#fasterFastPeopleSearchNavbarContent"
-            aria-controls="fasterFastPeopleSearchNavbarContent"
-            aria-expanded="true"
-            aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="fasterFastPeopleSearchNavbarContent">
-              <ul className="navbar-nav">
-                <li className="navbar-item">
-                  <Link className="nav-link" href='/fasterFastPeopleSearch/createSearch'>Create New Search</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link className="nav-link" href='/fasterFastPeopleSearch/searches'>Searches</Link>        
-                </li>
-              </ul>
-            </div>
+          <FasterFastPeopleSearchNavBar />
         </div>
       </div>
       {children}
+    </>
+  )
+}
+
+function FasterFastPeopleSearchNavBar() {
+  return (
+    <>
+       <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#fasterFastPeopleSearchNavbarContent"
+          aria-controls="fasterFastPeopleSearchNavbarContent"
+          aria-expanded="true"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      <div className="collapse navbar-collapse" id="fasterFastPeopleSearchNavbarContent">
+          <ul className="navbar-nav">
+            <li className="navbar-item">
+              <Link className="nav-link" href='/fasterFastPeopleSearch/createSearch'>Create New Search</Link>
+            </li>
+            <li className="navbar-item">
+              <Link className="nav-link" href='/fasterFastPeopleSearch/searches'>Searches</Link>        
+            </li>
+          </ul>
+        </div>
     </>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 import styles from './page.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapClient from './components/BootstrapClient';
-import AccountDropdown from './components/AccountDropdown';
+import AccountDropdown from './components/AccountDropdown.js';
 
 export const metadata = {
   title: 'Farm Friend',
@@ -14,36 +14,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="navbar-expanded">
         <nav className={`navbar`} data-bs-theme="dark">
           <div className="container-fluid">
-            <button 
-              className="navbar-toggler" 
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#navbarContent"
-              aria-controls="navbarContent"
-              aria-expanded="true"
-              aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+          <button
+            className="navbar-toggler" 
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="true"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`offcanvas offcanvas-start`} data-bs-backdrop="false" id="navbarContent">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Farm Friend</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className ="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" href='/fasterFastPeopleSearch'>Faster FastPeopleSearch</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
             <Link href='/'>
               <Image src='/farm-friend-logo.png' alt="Farm Friend Logo" width={100} height={100}/>
             </Link>
             <AccountDropdown />
-            <div className="offcanvas offcanvas-start show" id="navbarContent">
-              <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Farm Friend</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div className="offcanvas-body">
-                <ul className ="navbar-nav">
-                  <li className="nav-item">
-                    <Link className="nav-link" href='/fasterFastPeopleSearch'>Faster FastPeopleSearch</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
         </nav>
         <main className={styles.mainContent}>
