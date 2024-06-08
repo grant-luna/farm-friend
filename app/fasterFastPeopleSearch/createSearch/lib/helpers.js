0a120abea3aa3f5c608060c2eb7c812d4c7243f9
grant-luna <grantlunafidelity@gmail.com>
2024-06-07 20:35:08 -0700

    Recreate processFileForDatabase to accomodate new data structure of categories

--- app/fasterFastPeopleSearch/createSearch/lib/helpers.js
+++ app/fasterFastPeopleSearch/createSearch/lib/helpers.js
@@ -20,78 +20,19 @@ export function generateSampleRow(parsedCsvFile) {
   });
 }
 
-export function processFileForDatabase(parsedCsvFile, matchedColumnHeaders) {
-  function createFastPeopleSearchLink(pasredCsvFileRow, matchedColumnHeaders) {
-    const addressHeaders = matchedColumnHeaders["Address"];
-    const cityStateHeaders = matchedColumnHeaders["City / State"];
+export function processFileForDatabase(parsedCsvFile, categories) {
+  return parsedCsvFile.map((parsedCsvFileRow) => {
+    return categories.reduce((finalParsedCsvFileRow, category) => {
+      const categoryHeaders = Object.keys(category.headers);
 
-    const address = addressHeaders.map((addressHeader) => {
-      return pasredCsvFileRow[addressHeader]?.replace(/[^0-9a-z ]/gi, '');
-    }).join(' ')
+      finalParsedCsvFileRow[category.type] = categoryHeaders.reduce((finalHeadersObject, header) => {
+        finalHeadersObject[header] = category.headers[header].map((matchedHeader) => parsedCsvFileRow[matchedHeader]).join(' ');
+        return finalHeadersObject;
+      }, {});
 
-    const cityState = cityStateHeaders.map((cityStateHeader) => {
-      return pasredCsvFileRow[cityStateHeader];
-    }).join(' ');
-    
-    return `http://www.fastpeoplesearch.com/address/${address}_${cityState}`.replace(/ /g, '-');
-  }
-
-  return parsedCsvFile.map(function (parsedCsvFileRow) {
-    const primaryAddressLink = createFastPeopleSearchLink(parsedCsvFileRow, matchedColumnHeaders["Primary Address"]);
-    const mailAddressLink = createFastPeopleSearchLink(parsedCsvFileRow, matchedColumnHeaders["Mail Address"]);
-
-    return {
-      primaryAddressLink,
-      mailAddressLink,
-      primaryAddress: {
-        address: matchedColumnHeaders["Primary Address"]["Address"].map((header) => parsedCsvFileRow[header]).join(' '),
-        cityState: matchedColumnHeaders["Primary Address"]["City / State"].map((header) => parsedCsvFileRow[header]).join(' '),
-      },
-      mailAddress: {
-        address: matchedColumnHeaders["Mail Address"]["Address"].map((header) => parsedCsvFileRow[header]).join(' '),
-        cityState: matchedColumnHeaders["Mail Address"]["City / State"].map((header) => parsedCsvFileRow[header]).join(' '),
-      },
-      ownerNames: {
-        firstOwner: matchedColumnHeaders["Owner Names"]["First Owner"].map((header) => parsedCsvFileRow[header]).join(' '),
-        secondOwner: matchedColumnHeaders["Owner Names"]["Second Owner"].map((header) => parsedCsvFileRow[header]).join(' '),
-      }
-    }
-  });
-}
-
-export function resultsAreGenerateable(matchedColumnHeaderContext) {
-  const matchedColumnHeaders = matchedColumnHeaderContext.matchedColumnHeaders;
-  const matchedColumnHeaderKeysWithMatchedColumns = Object.keys(matchedColumnHeaders).filter((matchedColumnHeaderKey) => {
-    return Object.keys(matchedColumnHeaders[matchedColumnHeaderKey]).some((requiredHeader) => matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader].length > 0)
-  });
-  const onlyPrimaryAddressMatched = matchedColumnHeaderKeysWithMatchedColumns.length === 1 && matchedColumnHeaderKeysWithMatchedColumns[0] === 'Primary Address'
-  const multipleMatchedColumnHeaderKeysMatched = matchedColumnHeaderKeysWithMatchedColumns.length > 1;
-  
-  if (onlyPrimaryAddressMatched) {
-    const allPrimaryAddressRequiredHeadersMatched = Object.keys(matchedColumnHeaders["Primary Address"]).every((requiredHeader) => matchedColumnHeaders["Primary Address"][requiredHeader].length > 0);
-    
-    return allPrimaryAddressRequiredHeadersMatched ? true : false;
-  } else if (multipleMatchedColumnHeaderKeysMatched) {
-    let generatableStatus = true;
-
-    matchedColumnHeaderKeysWithMatchedColumns.forEach((matchedColumnHeaderKey) => {
-      if (matchedColumnHeaderKey === 'Owner Names') {
-        const atLeastOneOwnerNameMatched = Object.keys(matchedColumnHeaders["Owner Names"]).some((requiredHeader) => {
-          return matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader].length > 0;
-        });
-
-        if (!atLeastOneOwnerNameMatched) generatableStatus = false;
-      } else if (matchedColumnHeaderKey === 'Primary Address' || matchedColumnHeaderKey === 'Mail Address') {
-        const everyRequiredHeaderMatched = Object.keys(matchedColumnHeaders[matchedColumnHeaderKey]).every((requiredHeader) => {
-          return matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader].length > 0;
-        });
-
-        if (!everyRequiredHeaderMatched) generatableStatus = false;
-      }
-    });
-
-    return generatableStatus;
-  }
+      return finalParsedCsvFileRow;
+    }, {});
+  }, {});
 }
 
 export function updateInProgressStatus(currentCategoryCopy) {
--- app/fasterFastPeopleSearch/createSearch/page.jsx
+++ app/fasterFastPeopleSearch/createSearch/page.jsx
@@ -8,7 +8,6 @@ import {
   generateSampleRow,
   processFileForDatabase,
   generateHeaderSampleValue,
-  resultsAreGenerateable,
   updateInProgressStatus,
 } from './lib/helpers.js';
 import { createSearch } from '../actions/createSearch.js';
@@ -100,7 +99,13 @@ function FileProcessMenu() {
           className="btn btn-primary"
           data-bs-toggle="modal"
           data-bs-target="#categoryModal">
-          Get Started
+          Finalize Search Results
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          disabled={true}>
+          Choose Template
         </button>
         <FileProcessModal/>      
       </CategoriesContext.Provider>
@@ -116,6 +121,8 @@ function FileProcessModal() {
   const currentCategory = categories[currentPage];
   const { isGeneratable, setIsGeneratable } = useContext(SearchStatusContext);
   const navTabsRef = useRef(null);
+  const { parsedCsvFile, setParsedFile } = useContext(FileContext);
+  const [ readyForDatabase, setReadyForDatabase ] = useState(false);
 
   const stepInstructions = [
     <>
@@ -139,12 +146,10 @@ function FileProcessModal() {
   ];
 
   async function handleGenerateResults() {
-    if (isGeneratable) {
-      const processedFile = processFileForDatabase(parsedCsvFile, categories);
-      const searchData = await createSearch(JSON.stringify(processedFile));
-      const searchId = searchData.id;
-      router.push(`/fasterFastPeopleSearch/searches/${searchId}`);
-    }
+    const processedFile = processFileForDatabase(parsedCsvFile, categories);
+    debugger;
+    setParsedFile(processFileForDatabase(parsedCsvFile, categories));
+    setReadyForCheckout(true);
   }
 
   function handleHeaderClick(event) {
@@ -193,7 +198,7 @@ function FileProcessModal() {
       id="categoryModal" 
       aria-labelledby="categoryModalLabel"
       aria-hidden="true">
-        <div className={`${styles.modalDialog} modal-dialog modal-fullscreen modal-dialog-scrollable`}>
+        <div className={`${styles.modalDialog} modal-dialog modal-fullscreen modal-dialog-scrollable`} style={{top: '5%', width: '80%', height: '80%', margin: '0 auto'}}>
           <div className={`${styles.modalContent} modal-content`}>
             <div className="modal-header">
               <h5>{categories[currentPage].type} <span className="badge text-bg-info">{categories[currentPage].required ? ' Required' : 'Optional'}</span></h5>
@@ -348,110 +353,6 @@ function ColumnSelectorDropdown({ currentHeader, currentPage }) {
   )
 }
 
-
-function RequiredHeaderModal({ requiredHeader, matchedColumnHeaderKey, uniqueId }) {
-  const { parsedCsvFile } = useContext(FileContext);
-  const setIsGeneratable = useContext(SearchStatusContext);
-  const matchedColumnHeaderContext = useContext(MatchedColumnHeadersContext);
-  const sampleRow = generateSampleRow(parsedCsvFile);
-  const requiredHeaderSampleValue = generateRequiredHeaderSampleValue(matchedColumnHeaderContext.matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader], sampleRow)
-  const matchExamples = {
-    "Address": ["123 Main", "123 Main St", "123 S Main St", "123 S Main St #42"],
-    "City / State": ["Los Angeles, CA"],
-    "First Owner": ["Jane Doe", "Jane P Doe", "The Jane Doe Trust"],
-    "Second Owner": ["John Doe", "John P Doe", "The John P Doe Trust"],
-  };
-
-  function handleResetMatchedColumnHeaders(requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext, event) {
-    event.preventDefault();
-
-    const { matchedColumnHeaders, setMatchedColumnHeaders} = { ...matchedColumnHeaderContext };
-    setMatchedColumnHeaders({...matchedColumnHeaders, [matchedColumnHeaderKey]: {...matchedColumnHeaders[matchedColumnHeaderKey], [requiredHeader]: []}});
-    if (matchedColumnHeaderKey === 'Primary Address') setIsGeneratable(false);
-  }
-
-  function handleSampleColumnClick(requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext, event) {
-    const isSampleColumn = event.target.closest('li').classList.contains('sample-column')
-
-    if (isSampleColumn) {
-      event.preventDefault();
-
-      const { matchedColumnHeaders, setMatchedColumnHeaders} = { ...matchedColumnHeaderContext };
-      const matchedColumnHeaderKeyMatchedColumns = [...matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader]];
-      const sampleColumn = event.target.closest('li');
-      const [ sampleColumnHeader, sampleColumnValue ] = [...sampleColumn.children].map((child) => child.textContent);
-      matchedColumnHeaderKeyMatchedColumns.push(sampleColumnHeader);
-      
-      setMatchedColumnHeaders({...matchedColumnHeaders, [matchedColumnHeaderKey]: {...matchedColumnHeaders[matchedColumnHeaderKey], [requiredHeader]: matchedColumnHeaderKeyMatchedColumns}});
-    }
-  }
-
-  function handleSampleColumnHover(event) {
-    const sampleColumn = event.currentTarget;
-
-    sampleColumn.classList.add('active');
-    sampleColumn.style.cursor = 'pointer';
-  }
-
-  function handleSampleColumnUnHover(event) {
-    const sampleColumn = event.currentTarget;
-
-    sampleColumn.classList.remove('active');
-    sampleColumn.style.cursor = 'default';
-  }
-
-  function handleSaveMatchedColumnHeaders(requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext) {
-    const requiredHeaderInput = [...document.querySelectorAll(`.accordion-body .form-check-input`)].filter((requiredHeaderInput) => {
-      return requiredHeaderInput.nextElementSibling.textContent === requiredHeader;
-    }).find((requiredHeaderInput) => {
-      const accordionHeaderText = requiredHeaderInput.closest('li.accordion-item').querySelector('h2.accordion-header')?.textContent;
-      return matchedColumnHeaderKey === accordionHeaderText;
-    });
-
-    const requiredHeaderColumnsMatched = matchedColumnHeaderContext.matchedColumnHeaders[matchedColumnHeaderKey][requiredHeader].length > 0;
-    requiredHeaderInput.checked = requiredHeaderColumnsMatched ? true : false;
-
-    setIsGeneratable(resultsAreGenerateable(matchedColumnHeaderContext) ? true : false)
-  }
-
-  return (
-    <div className="modal fade" data-bs-backdrop="static" id={uniqueId} aria-hidden="true">
-      <div className="modal-dialog modal-fullscreen modal-dialog-centered modal-dialog-scrollable">
-        <div className="modal-content">
-          <div className="modal-header">
-              <h5 className="modal-title">Matching {requiredHeader}</h5>
-              <button onClick={handleSaveMatchedColumnHeaders.bind(null, requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext)} type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <div className={`${styles.matchExamplesContainer}`}>
-                <h6>Here are examples of what we&#39;re looking for:</h6>
-                <ul className={`${styles.matchExampleList}`}>
-                  {matchExamples[requiredHeader].map((matchExample, index) => {
-                    return <li key={index} className="badge text-bg-info">{matchExample}</li>
-                  })}
-                </ul>
-              </div>
-              <h6>Current Match: {requiredHeaderSampleValue}</h6>
-              <ul className={`${styles.sampleColumns} list-group`} onClick={handleSampleColumnClick.bind(null, requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext)}>
-                {sampleRow.map((column, index) => (
-                  <li
-                      onMouseOver={handleSampleColumnHover}
-                      onMouseLeave={handleSampleColumnUnHover}
-                      className={`${styles.sampleColumn} list-group-item sample-column`}
-                      key={index}
-                    >
-                    <h4><strong>{column.header}</strong></h4>
-                    <p>{column.value}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="modal-footer">
-              <button onClick={handleResetMatchedColumnHeaders.bind(null, requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext)} type="button"  className="btn btn-light">Reset</button>
-              <button onClick={handleSaveMatchedColumnHeaders.bind(null, requiredHeader, matchedColumnHeaderKey, matchedColumnHeaderContext)} type="button" data-bs-dismiss="modal" className="btn btn-primary">Save</button>
-            </div>
-          </div>
-      </div>
-    </div>
-  )
+function ModalCheckoutForm({}) {
+  
 }
