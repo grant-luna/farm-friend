1f67f4d71d54a320c57fd7e7346e3902b4c86796
grant-luna <grantlunafidelity@gmail.com>
2024-06-17 17:19:18 -0700

    Make style changes to SearchItem in fasterFastPeopleSearch/searches

--- app/fasterFastPeopleSearch/searches/page.jsx
+++ app/fasterFastPeopleSearch/searches/page.jsx
@@ -2,7 +2,9 @@
 import { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { fetchSearches } from '../actions/fetchSearches.js';
-import { BsFillTrash3Fill } from "react-icons/bs";
+import Link from 'next/link'
+import { MdDeleteOutline } from "react-icons/md";
+import { HiDotsVertical } from "react-icons/hi";
 
 export default function MainContent() {
   return (
@@ -33,8 +35,7 @@ function SearchesContainer() {
     (async () => {
       try {
         const fetchedSearches = await fetchSearches();
-        maxPages.current = Math.ceil((fetchedSearches.length / 10));
-        debugger;       
+        maxPages.current = Math.ceil((fetchedSearches.length / 10));      
         setSearches(fetchedSearches);        
         setLoading(false);
       } catch (error) {
@@ -65,7 +66,7 @@ function SearchesContainer() {
     <>
       {loading && <p>Loading Your Searches...</p>}
       {!loading && <div>
-        <ul className="d-flex flex-wrap justify-content-center" style={{gap: '.75rem'}}>
+        <ul className="d-flex flex-wrap justify-content-center" style={{gap: '.25rem'}}>
           {visibleSearches.map((search, index) => {
             return <SearchItem key={index} search={search}/>
           })}
@@ -98,41 +99,103 @@ function SearchesContainer() {
 }
 
 function SearchItem({ search }) {
+  const [ searchItemHovered, setSearchitemHovered ] = useState(false);
   const router = useRouter();
 
   function handleDeleteSearch() {
     
   }
 
-  function handleSearchItemClick(searchId) {
+  function handleLinkMouseEnter(event) {
+    if (event.target === event.currentTarget) {
+      event.target.style.textDecoration = 'underline';
+      event.target.style.cursor = 'pointer'; // Corrected property name
+    }    
+  }
+
+  function handleLinkMouseLeave(event) {
+    if (event.target === event.currentTarget) {
+      event.target.style.textDecoration = 'none';
+      event.target.style.cursor = 'pointer'; // Corrected property name
+    }
+  }
+
+  function handleSearchItemMouseEnter(event) {
+    event.currentTarget.style.backgroundColor = '#F5F8FA';
+    event.currentTarget.style.cursor = 'pointer';    
+    setSearchitemHovered(true);
+  }
+
+  function handleSearchItemMouseLeave(event) {
+    event.currentTarget.style.backgroundColor = 'white';
+    setSearchitemHovered(false);
+  }
+
+  function handleSearchItemClick(searchId, event) {
+    event.preventDefault();
     router.push(`/fasterFastPeopleSearch/searches/${searchId}`);
   }
   
   return (
-    <li className={`card d-flex flex-column align-items-start`} style={{padding: '.5rem'}}>
-      <div className="card-body">
-        <h4 className="card-title">{search["search_name"]}</h4>
-      </div>
-      <div className="card-body">
+    <li
+      className="d-flex flex-column align-items-start"
+      style={{border: '1px solid grey', padding: '.5rem', borderRadius: '.25rem', gap: '.25rem'}}
+      onClick={handleSearchItemClick.bind(null, search.id)}
+      onMouseEnter={handleSearchItemMouseEnter}
+      onMouseLeave={handleSearchItemMouseLeave}>
+      <div className="d-flex flex-column align-items-start">
+        <div className="d-flex align-items-start justify-content-between" style={{width: '100%'}}>
+          <h6>
+            <Link
+              style={{color: '#0091AE', textDecoration: 'none'}}
+              href="#"
+              onMouseEnter={handleLinkMouseEnter}
+              onMouseLeave={handleLinkMouseLeave}>
+              {search["search_name"]}
+            </Link>
+          </h6>
+          <div className="d-flex justify-items-end align-items-center">
+            <HiDotsVertical />
+          </div>          
+        </div>
         <ul className="list-group" style={{textAlign: 'start'}}>
           <li className="list-group-item"><strong>Date Created: </strong>{search["created_at"].toDateString()}</li>
           <li className="list-group-item"><strong>Number of Contacts: </strong>{search["search_data"].length}</li>
         </ul>
       </div>
-      <div className="d-flex justify-content-between align-items-center" style={{gap: '.5rem', width: '100%'}}>
-        <button
-          className="btn btn-success"
-          onClick={handleSearchItemClick.bind(null, search.id)}>
-          See Contacts
-        </button>
-        <button 
-          className="btn btn-light"
-          type="button"
-          data-bs-toggle="modal"
-          data-bs-target={`#delete`}>
-          <BsFillTrash3Fill />
-        </button>
+      <div style={{width: '90%', outline: '1px solid grey', margin: '0 auto'}}></div>
+      <div className="d-flex align-items-center">
+
       </div>
     </li>
   )
 }
+
+/*
+ <li className={`card d-flex flex-column align-items-start`} style={{padding: '.5rem'}}>
+  <div className="card-body d-flex align-items-start justify-content-between" style={{width: '100%'}}>
+    <h4 className="card-title">{search["search_name"]}</h4>
+    <BsThreeDotsVertical />
+  </div>
+  <div className="card-body">
+    <ul className="list-group" style={{textAlign: 'start'}}>
+      <li className="list-group-item"><strong>Date Created: </strong>{search["created_at"].toDateString()}</li>
+      <li className="list-group-item"><strong>Number of Contacts: </strong>{search["search_data"].length}</li>
+    </ul>
+  </div>
+  <div className="d-flex justify-content-between align-items-center" style={{gap: '.5rem', width: '100%'}}>
+    <button
+      className="btn btn-success"
+      onClick={handleSearchItemClick.bind(null, search.id)}>
+      See Contacts
+    </button>
+    <button 
+      className="btn btn-light"
+      type="button"
+      data-bs-toggle="modal"
+      data-bs-target={`#delete`}>
+      <BsFillTrash3Fill />
+    </button>
+  </div>
+</li>
+*/
